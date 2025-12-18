2215155b2c3340d2ed26c190401ac293d59889dc
Durba <durba.biswas1807@gmail.com>
2025-12-18 09:02:27 +0530

    Implement maxSubArray method for maximum sum

---
+++ practice/Max-Sum-Contiguous-Subarray/code.java
@@ -0,0 +1,18 @@
+public class Solution {
+    // DO NOT MODIFY THE ARGUMENTS WITH "final" PREFIX. IT IS READ ONLY
+    public int maxSubArray(final int[] A) {
+        int N = A.length;
+        int maxsum = Integer.MIN_VALUE;
+        int sum = 0;
+
+        for (int i = 0; i < N; i++)
+        {
+            sum += A[i];
+            if (sum > maxsum) maxsum = sum;
+
+            if (sum < 0) sum = 0;
+        }
+
+        return maxsum;
+    }
+}
